0a9ebe5b2759423ce4ac4f00cf04187f6abc6881
Alma <ardaraviciute@yahoo.com>
2023-03-13 00:11:41 +0200

    literals explained

--- src/com/sda/she_likes_java/operators/Operators.java
+++ src/com/sda/she_likes_java/operators/Operators.java
@@ -50,6 +50,23 @@ public class Operators {
             System.out.println("You are not polite, or just naughty");
         }
 
+        System.out.println();
+        System.out.println("Let's see now || operator - or operator");
+        boolean hasGun = true;
+        boolean hasGrenade = true;
+        boolean isTerrorist = hasGun || hasGrenade;
+        System.out.println("true || true is: " +isTerrorist);
+
+        hasGun = true;
+        hasGrenade = false;
+        isTerrorist = hasGun || hasGrenade;
+        System.out.println("true || false is: " +isTerrorist);
+
+        hasGun = false;
+        hasGrenade = false;
+        isTerrorist = hasGun || hasGrenade;
+        System.out.println("false || false is: " +isTerrorist);
+
 
     }
 }
