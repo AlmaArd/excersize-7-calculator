5ce7426d6a45547530302a19f942cc692f5803f8
Alma <ardaraviciute@yahoo.com>
2023-03-17 17:28:23 +0200

    guess the number game - with break, needs to be finished

--- src/com/sda/she_likes_java/loops/do_while/GuessTheNumberGameWithBreak.java
+++ src/com/sda/she_likes_java/loops/do_while/GuessTheNumberGameWithBreak.java
@@ -3,18 +3,23 @@ package com.sda.she_likes_java.loops.do_while;
 import java.util.Random;
 import java.util.Scanner;
 
-public class GuessTheNumber {
+public class GuessTheNumberGameWithBreak {
     public static void main(String[] args) {
         Random numberGenerator = new Random();
-        int someRandomNumber = numberGenerator.nextInt(10);
-//        System.out.println("Random number provided by Java is: " +someRandomNumber);
+        int someRandomNumber = numberGenerator.nextInt(20);
+        int numberOfTrials = 8;
+        System.out.println("Random number provided by Java is: " +someRandomNumber);
 
         Scanner inputReader = new Scanner(System.in);
         int userAnswer;
         System.out.println("Play game with me:)");
         do {
-        System.out.println("Guess a number between 1 and 10");
+            if (numberOfTrials <= 0) {
+                break;
+            }
+        System.out.println("Guess a number between 1 and 20");
         userAnswer = inputReader.nextInt();
+        numberOfTrials--;
         } while (userAnswer != someRandomNumber);
 
         System.out.println("Great, You win");
