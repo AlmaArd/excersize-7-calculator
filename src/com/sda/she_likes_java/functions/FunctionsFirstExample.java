01c09a40063c779b1cb9519101f4f4011b10c0f8
Alma <ardaraviciute@yahoo.com>
2023-03-17 08:53:30 +0200

    function - first example

--- src/com/sda/she_likes_java/functions/FunctionsFirstExample.java
+++ src/com/sda/she_likes_java/functions/FunctionsFirstExample.java
@@ -1,12 +1,14 @@
 package com.sda.she_likes_java.functions;
 
 public class FunctionsFirstExample {
+    // it's function
     public static void main(String[] args) {
         // it's a piece of the code which I can call many times
         // function features
         // has name - just before opening bracket like "main"
         // has parameters - inside brackets String[] args
         // has return value - type name before name of the function e.g.void
+        // void means no return
         // we call function by its name
         printMyName();
 
@@ -17,6 +19,7 @@ public class FunctionsFirstExample {
         printMyName();
     }
 
+    // it's function
     public static void printMyName () {
         int numberOfTimes = 10;
         while (numberOfTimes > 0) {
